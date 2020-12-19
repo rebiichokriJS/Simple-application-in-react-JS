24acbcd4b509c1b5c7c8f9836fa88cc8d9c05f99
Rebii Chokri <rebiichokri@gmail.com>
2020-12-19 16:30:05 +0100

    simple application in react JS

---
+++ build/precache-manifest.1f987e1e5bab4b6b843276aac8c70292.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85a1190997fed92df0a8f212cf8ac464",
+    "url": "/index.html"
+  },
+  {
+    "revision": "750850db1f4e27db47ca",
+    "url": "/static/css/2.8e1873ca.chunk.css"
+  },
+  {
+    "revision": "0a1e3d5f4d14419d5db3",
+    "url": "/static/css/main.9f220fe0.chunk.css"
+  },
+  {
+    "revision": "750850db1f4e27db47ca",
+    "url": "/static/js/2.220292aa.chunk.js"
+  },
+  {
+    "revision": "0a1e3d5f4d14419d5db3",
+    "url": "/static/js/main.2d782fed.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
